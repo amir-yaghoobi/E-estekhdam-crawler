5fa75bb2a14cceaeade466187c5725df21d7fd4a
Amirhossein Yaghoobi <a.yaghoobi.uni@gmail.com>
2018-03-13 08:52:17 +0330

    init

---
+++ config.py
@@ -0,0 +1,11 @@
+# E-estekhdam url
+# value must be valid url
+SITE_URL = 'http://www.e-estekhdam.com/search/استخدام-برنامه-نویس-در-تهران/'
+
+# Limit on how many pages crawl into
+# value must be an integer and more than zero
+PAGE_LIMIT = 10
+
+# Keywords to look into
+# value must be an list and have more than zero items
+KEYWORDS = ['کارآموز',]
